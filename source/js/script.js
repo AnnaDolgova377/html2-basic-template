67d71ce3dd7384aa56f4b364582983d80481792f
Anna Lopatina <anjuta377@gmail.com>
2023-06-14 21:45:29 +0300

    зависимости

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,18 @@
-/* в этот файл добавляет скрипты*/
+let nav = document.querySelector('.navigation');
+let navToogle = document.querySelector('.navigation__toggle');
+let map = document.querySelector('.footer-address__wrapper-location');
+
+nav.classList.remove('navigation--nojs');
+
+navToogle.addEventListener('click', function () {
+  if (nav.classList.contains('navigation--closed')) {
+    nav.classList.remove('navigation--closed');
+    nav.classList.add('navigation--opened');
+  } else {
+    nav.classList.add('navigation--closed');
+    nav.classList.remove('navigation--opened');
+  }
+});
+
+map.classList.remove('footer-address__wrapper-location--nojs');
+
